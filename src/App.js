12c13f7392d214b895cf83843052c829b6b04509
julhanlon <jhanlon289@gmail.com>
2020-08-18 20:24:55 -0700

    ployees are showing up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from "../src/components/Nav.js"
 import Employees from "./components/Employees.js"
 import { Container, Row, Col } from "./components/Grid";
+import Filter from "./components/Filter.js"
 
 function App() {
 
@@ -10,12 +11,18 @@ function App() {
     <div>
    <Nav/>
    <Container>
-                <Row>
-                  <Col size="xs-12 lg-6">
-                    <Employees/>
-                  </Col>
-                </Row>
-              </Container>
+        <Row>
+        <Col size="xs-12 lg-2">
+                <Filter/>
+            </Col>
+            <Col size="xs-12 lg-5">
+                <Employees/>
+            </Col>
+            <Col size="xs-12 lg-5">
+                <Employees/>
+            </Col>
+        </Row>
+    </Container>
     </div>
   );
 }
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default {
+  //gets all employees
   getEmployees: function() {
     return axios.get("https://randomuser.me/api/?results=6" );
   }
