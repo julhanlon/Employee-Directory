19bc4bb4e05fcd9fb38b17133d028001e863dca4
julhanlon <jhanlon289@gmail.com>
2020-08-18 20:56:04 -0700

    changes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Button from "./Button"
 
 const Filter = () => {
     return (
-        <div>
-            <h1>button area</h1>
+        <div className = "mt-5">
+            <div>
+                <h3>Sort by Last Name</h3>
+            </div>
+           {/* <Button onclick = {()}></Button> */}
         </div>
     )
 }
