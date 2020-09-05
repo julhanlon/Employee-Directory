e53c8a42c53093cca6224f311d819f5adfd81b76
julhanlon <jhanlon289@gmail.com>
2020-09-05 11:07:11 -0700

    changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
+const divStyle = {
+  height: "100px"
+}
+
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary" style = {divStyle}>
       <a className="navbar-brand" href="/">
         Employee Directory
       </a>
